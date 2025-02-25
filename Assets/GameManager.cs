0fced3ef489ad0bdbf2bcdc538b81626931104b0
WillBoudreau <williamboudreau804@gmail.com>
2025-02-24 19:11:31 -0500

    Targets rotate towards the player

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [Header("Game Manager Settings")]
-    [SerializeField] private GameObject[] BallPrefabs; // List of ball prefabs to spawn
-    [SerializeField] private Transform[] BallSpawnPoints; // List of spawn points for the balls
+    [SerializeField] private GameObject[] ballPrefabs; // List of ball prefabs to spawn
+    [SerializeField] private Transform[] ballSpawnPoints; // List of spawn points for the balls
+    [SerializeField] private GameObject[] targetPrefabs; // List of target prefabs to spawn
+    [SerializeField] private Transform[] targetSpawnPoints; // List of spawn points for the targets
     [SerializeField] private float spawnTime = 5; // Time between ball spawns
     [SerializeField] private int maxBalls = 5; // Maximum number of balls that can be spawned
+    [SerializeField] private int maxTargets = 5; // Maximum number of targets that can be spawned
+
 
     void Update()
     {
         SpawnBall();
+        SpawnTarget();
     }
     /// <summary>
     /// Spawn a ball at the given position
@@ -24,12 +29,30 @@ public class GameManager : MonoBehaviour
             return;
         }
         // Get a random ball prefab
-        GameObject ballPrefab = BallPrefabs[Random.Range(0, BallPrefabs.Length)];
+        GameObject ballPrefab = ballPrefabs[Random.Range(0, ballPrefabs.Length)];
 
         // Get a random spawn point
-        Transform spawnPoint = BallSpawnPoints[Random.Range(0, BallSpawnPoints.Length)];
+        Transform spawnPoint = ballSpawnPoints[Random.Range(0, ballSpawnPoints.Length)];
 
         // Spawn the ball at the spawn point
         Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity);
+    }
+    /// <summary>
+    /// Spawn a target at the given position
+    /// </summary>
+    void SpawnTarget()
+    {
+        if(GameObject.FindGameObjectsWithTag("Target").Length >= maxTargets)
+        {
+            return;
+        }
+        // Get a random target prefab
+        GameObject targetPrefab = targetPrefabs[Random.Range(0, targetPrefabs.Length)];
+
+        // Get a random spawn point
+        Transform spawnPoint = targetSpawnPoints[Random.Range(0, targetSpawnPoints.Length)];
+
+        // Spawn the target at the spawn point
+        Instantiate(targetPrefab, spawnPoint.position, Quaternion.identity);
     }
 }
---
+++ Assets/TargetBehaviour.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    [Header("Target Settings")]
+    [SerializeField] private GameObject player; // The player object
+    [Header("Target Rotation Settings")]
+    [SerializeField] private float rotationSpeed = 1.0f; // Rotation speed multiplier
+    [SerializeField] private float zRotation = 90; // Z rotation of the target
+    [SerializeField] private float yRotation = 0; // Y rotation of the target
+    [SerializeField] private float xRotation = 0; // X rotation of the target
+
+    void Start()
+    {
+        FindPlayer();
+    }
+    void Update()
+    {
+        RotateTowardsPlayer();
+    }
+
+    /// <summary>
+    /// Find the player object
+    /// </summary>
+    void FindPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    /// <summary>
+    /// Rotate the target towards the player
+    /// </summary>
+    void RotateTowardsPlayer()
+    {
+        // Get the direction to the player
+        Vector3 direction = player.transform.position - transform.position;
+
+        // Get the rotation to look at the player
+        Quaternion rotation = Quaternion.LookRotation(direction);
+
+        // Adjust the rotation to be a perfect 
+        rotation *= Quaternion.Euler(xRotation, yRotation, zRotation);
+
+        // Rotate the target towards the player
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+    }
+}
